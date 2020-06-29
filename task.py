38fdde5a2bd4dae173eb7165acd71bdf4b81f75e
RexRode <61255136+RexRode@users.noreply.github.com>
2020-06-29 17:10:48 -0400

    Add files via upload

--- task.py
+++ task.py
@@ -1,12 +1,8 @@
-class Lightbulb:
-    def __init__(self):
-        self.state = "off"
-
-    # create method change_state here
-    def change_state(self):
-        if self.state == "off":
-            self.state = "on"
-            print("Turning the light on")
-        elif self.state == "on":
-            self.state = "off"
-            print("Turning the light off")
+# Make sure your output matches the assignment *exactly*
+time_spent_on_computer_in_hours_per_day = int(input())
+if time_spent_on_computer_in_hours_per_day < 2:
+    print("That seems reasonable")
+elif time_spent_on_computer_in_hours_per_day < 4:
+    print("Do you have time for anything else?")
+else:
+    print("You need to get outside more!")
